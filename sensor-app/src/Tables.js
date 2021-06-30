1ee9f981e1894614ee39c28f599618a9e4f5d521
Madushi-deSilva <madushi97pabasara@gmail.com>
2021-06-30 09:20:35 +0530

    table created

--- sensor-app/src/Tables.js
+++ sensor-app/src/Tables.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import Table from 'react-bootstrap/Table'
 import Sidebar from './Sidebar';
 
 function Tables () {
@@ -9,7 +10,43 @@ function Tables () {
                 {/* ----------sidebar inserted---------------- */}
                 <Sidebar/>
                 {/* ---------------home page------------ */}
-                <div className="homeDiv col-10 text-center" style={{height:'88vh'}}></div>
+                <div className="homeDiv col-10 text-center" style={{height:'88vh'}}>
+                        <div className="row">
+                            <h1 style={{fontFamily:'serif', padding:'10px'}}>ALERT DETAILS</h1>
+                        </div>
+                        <div className="row" style={{margin:'10px'}}>
+                            <Table  bordered hover responsive>
+                                <thead style={{backgroundColor:'#03bafc', borderTop:'2px solid black'}}> 
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Date</th>
+                                        <th>Message</th>
+                                        <th>Receiver</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>2021-01-21</td>
+                                        <td>Hiiii</td>
+                                        <td>Kamal</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>2021-01-21</td>
+                                        <td>Hiiii</td>
+                                        <td>Kamal</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>2021-01-21</td>
+                                        <td>Hiiii</td>
+                                        <td>Kamal</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </div> 
+                    </div>
          </div>
          );
 }
