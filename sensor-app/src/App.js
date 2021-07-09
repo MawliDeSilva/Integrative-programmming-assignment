f97438395f1024b221132585923d7d131ed02f37
SachiniGinige <SachiniGinige@users.noreply.github.com>
2021-07-10 04:03:00 +0530

    edit adduser form

--- sensor-app/src/App.js
+++ sensor-app/src/App.js
@@ -4,6 +4,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import HomeSensor from './HomeSensor';
 import Tables from './Tables';
+import AddUser from './adduser.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch> 
           <Route path='/' exact component={HomeSensor}/>
           <Route path='/tables' exact component={Tables}/>
+          <Route path='/user-add' component={AddUser}/>
         </Switch>
     </Router>
         
---
+++ sensor-app/src/adduser.component.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import axios from "axios";
+import './App.css';
+
+export default class AddUser extends Component{
+    constructor(props){
+        super(props);
+
+        // this.onChangeUserId=this.onChangeUserId.bind(this);
+        this.onChangeName=this.onChangeName.bind(this);
+        this.onChangeType=this.onChangeType.bind(this);
+        this.onChangeContactInfo=this.onChangeContactInfo.bind(this);
+
+        this.onSubmit=this.onSubmit.bind(this);
+
+        this.state = {
+            // uid: '',
+            name: '',
+            type: '',
+            contactInfo: '',
+        }
+    }
+
+    // onChangeUserId(e){
+    //     this.setState({
+    //         uid: e.target.value
+    //     });
+    // }
+    onChangeName(e){
+        this.setState({
+            name: e.target.value
+        });
+    }
+    onChangeType(e){
+        this.setState({
+            type: e.target.value
+        });
+    }
+    onChangeContactInfo(e){
+        this.setState({
+            contactInfo: e.target.value
+        });
+    }
+    
+    onSubmit(e){
+        e.preventDefault();
+
+        const user = {
+            // uid:  this.state.uid,
+            name:  this.state.name,
+            type:  this.state.type,
+            contactInfo:  this.state.contactInfo,
+        }
+
+        console.log(user);
+
+        axios.post('http://localhost:8080/users/add', user)
+            .then(res => console.log(res.data));
+        
+        alert("User added");            
+    }
+
+    render() {
+        return(
+              <div className="container">
+                <h1>Add User</h1>
+                <form>        
+{/* 
+                <label for="name"><b>User ID:</b></label>
+                <input type="text" placeholder="User ID" name="uid" className="form-control" value={this.state.uid} onChange={this.onChangeUserId} required/>
+                <br/><br/> */}
+
+                <label for="name"><b>Name:</b></label>
+                <input type="text" placeholder="Name" name="name" className="form-control" value={this.state.name} onChange={this.onChangeName} required/>
+                <br/><br/>
+
+                <label for="PreComChan"> <b> Prefered Communication Channel: </b></label>    
+                <select id="PreComChan" className="form-select" value={this.state.type} onChange={this.onChangeType}>
+                    <option name="typeEmail" value="Email">Email</option>
+                    <option name="typeSMS" value="SMS">SMS</option>
+                    <option name="typeCall" value="Call">Call</option>         
+                </select>
+                <br/><br/>
+
+                <label><b>Contact Info:</b></label>
+                <input type="text" name="Info" placeholder="Contact Info" className="form-control" value={this.state.contactInfo} onChange={this.onChangeContactInfo} required/>
+                <br/><br/>
+
+                <div className="clearfix">
+                <button type="cancel" className="btn btn-primary">Cancel</button>
+                <button type="submit" className="btn btn-primary" onClick={this.onSubmit}>Add User</button>    
+                </div>
+            </form>
+            </div>               
+        );
+    }
+}
