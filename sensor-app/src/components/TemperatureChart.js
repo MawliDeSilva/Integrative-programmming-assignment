f538819010efbbe85e3d274ef76eac6d92b429ab
Nirasha <jkwapcnrj@gmail.com>
2021-07-13 21:48:21 +0530

    Update TemperatureChart.js

--- sensor-app/src/components/TemperatureChart.js
+++ sensor-app/src/components/TemperatureChart.js
@@ -1,53 +1,50 @@
 import React,{ useState,useEffect } from 'react';
 import{Bar,Line, Pie} from 'react-chartjs-2';
-// import{Bar,Line, Pie} from 'chart.js';
-import { Component } from 'react';
-import TempRecordTable from './TempReadingTable';
 import axios from 'axios';
 
-// class TemperatureChart extends Component{
+
 const TemperatureChart =()=>{
     
     const[chartData, setChartData] = useState({});
     const[temperatureData, setTemperatureData] = useState([]);
     const[timeSeriesData, setTimeData] = useState([]);
-    const Chart =()=>{
+    
 
+    const Chart =()=>{
         let TempData =[];
         let TimeData =[];
-
-        // axios.get("http://localhost:8080/tempreadings/")
         axios.get("http://localhost:8080/tempreadings/")
         .then(response => {
             console.log(response) 
+            
             //  for (const dataObj of response.data.data){
             for (const dataObj of response.data){
                 TempData.push(parseFloat(dataObj.data_value))
                 TimeData.push(parseFloat(dataObj.date))
             }
+            setChartData({
+                // chartData :{
+                    labels:TimeData,
+                    datasets: [{
+                        label: 'Temperature data',
+                        data: TempData,
+                        fill: false,
+                        borderColor: 'rgb(75, 192, 192)',
+                        tension: 0.1
+                    }]
+            });
         })
         .catch((error) =>{
             console.log(error);
         })
         console.log(TempData, TimeData)
 
-        setChartData({
-            chartData :{
-                labels:TimeData,
-                datasets: [{
-                    label: 'Temperature data',
-                    data: TempData,
-                    fill: false,
-                    borderColor: 'rgb(75, 192, 192)',
-                    tension: 0.1
-                }]
-            }
-        })
+        
     }
 
     useEffect(() => {
-        Chart()
-    },[])
+        Chart();
+    },[]);
 // }
     // constructor(props){
     //     super(props);
@@ -80,7 +77,7 @@ const TemperatureChart =()=>{
                 data ={chartData}
                 options = {{
                     title :{
-                        display: true,
+                        display: false,
                         text:'Temperature Variation',
                         fontSize : 25,
             
